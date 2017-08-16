a7ce07632a845bf9d6ad53ee0d10d8e883a8796c
WeizhuoZHANG <weizhuo.zhang@qq.com>
2017-08-16 21:08:55 +1000

    Add Query.java

---
+++ AI_Assignment_1/src/bean/Query.java
@@ -0,0 +1,40 @@
+package bean;
+
+public class Query {
+	private String initName;
+	private String endName;
+	private int initPlot;
+	private int endPlot;
+
+	public String getInitName() {
+		return initName;
+	}
+
+	public void setInitName(String initName) {
+		this.initName = initName;
+	}
+
+	public String getEndName() {
+		return endName;
+	}
+
+	public void setEndName(String endName) {
+		this.endName = endName;
+	}
+
+	public int getInitPlot() {
+		return initPlot;
+	}
+
+	public void setInitPlot(int initPlot) {
+		this.initPlot = initPlot;
+	}
+
+	public int getEndPlot() {
+		return endPlot;
+	}
+
+	public void setEndPlot(int endPlot) {
+		this.endPlot = endPlot;
+	}
+}
